3ca8358056b9654b4fe672b6b475e57421fe60f5
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2023-12-05 06:23:08 +0000

    Create schema_users.ps1

---
+++ schema_users.ps1
@@ -0,0 +1,27 @@
+$userpass="elastic:yourpassword"
+$userbyte=[System.Text.Encoding]::ASCII.GetBytes($userpass)
+$b64text=[System.Convert]::ToBase64String($userbyte)
+$authHeader="Basic $b64text"
+$headers=@{
+    Authorization=$authHeader
+    "Content-Type"="application/x-ndjson"
+}
+
+$body=@"
+{
+    "mappings":{
+        "properties":{
+            "Item":{"type":"integer"},
+            "ipaddr":{"type":"ip"},
+            "username":{"type":"text"},
+            "useragent":{"type":"text"},
+            "Message":{"type":"text"}
+        }
+    }
+}
+"@
+
+$output = Invoke-RestMethod -Uri "https://172.31.24.22:9200/suspectusers" -Method Put -Headers $headers -Body $body -SkipCertificateCheck
+
+$showschema = Invoke-RestMethod -Uri "https://172.31.24.22:9200/suspectusers/_mapping" -Method Get -Headers $headers -SkipCertificateCheck
+$showschema | ConvertTo-Json -Depth 5
