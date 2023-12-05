5981a5c396b1bdfe29d3b15e6e8ed6bfca60cf10
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2023-12-05 04:18:13 +0000

    Create connect.ps1

---
+++ connect.ps1
@@ -0,0 +1,13 @@
+$userpass="elastic:yourpassword"
+$userbyte=[System.Text.Encoding]::ASCII.GetBytes($userpass)
+$b64text=[System.Convert]::ToBase64String($userbytes)
+$authHeader="Basic $b64text"
+$headers=@{
+    Authorization=$authHeader
+    "Content-Type"="application/x-ndjson"
+}
+
+$output = Invoke-RestMethod -Uri "https://172.31.24.22:9200" -Method Get -Headers $headers -SkipCertificateCheck
+
+$output
+
