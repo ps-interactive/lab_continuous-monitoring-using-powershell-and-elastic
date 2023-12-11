c983ef41297fd304c7181b79111a4688ac5cf3ac
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2023-12-11 08:42:14 +0000

    .

---
+++ winlogs.ps1
@@ -0,0 +1,19 @@
+$userpass="elastic:yourpassword"
+$userbyte=[System.Text.Encoding]::ASCII.GetBytes($userpass)
+$b64text=[System.Convert]::ToBase64String($userbyte)
+$authHeader="Basic $b64text"
+$headers=@{
+    Authorization=$authHeader
+    "Content-Type"="application/x-ndjson"
+}
+
+$winlogs = Import-CSV /home/pslearner/lab/defenderlogs.txt
+
+$logs.ForEach({
+    $body = ConvertTo-Json $_
+    Write-Host $body
+})
+
+$output = Invoke-RestMethod -Uri "https://172.31.24.22:9200/windowslogs/_doc" -Method Post -Headers $headers -Body $body -SkipCertificateCheck
+
+})
